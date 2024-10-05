3be022df6f87b818344ab7e8da49e3b7cb4f50ca
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2024-10-05 16:19:37 +0300

    Update hovering UI

--- features/object_interacting/ui/object_hover_ui_controller.gd
+++ features/object_interacting/ui/object_hover_ui_controller.gd
@@ -1,15 +1,17 @@
 extends Node
 
-@onready var label: Label = $center_container/margin/hoverable_name
+@onready var panel: PanelContainer = $"center_container/margin/panel"
+@onready var label: Label = $"center_container/margin/panel/margin/hoverable_name"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	GameManager.current_gameplay.on_interactable_hovered.connect(on_hovered)
-	pass # Replace with function body.
+	if GameManager.current_gameplay:
+		GameManager.current_gameplay.on_interactable_hovered.connect(on_hovered)
 
 func on_hovered(interactable: InteractableObject):
 	if interactable:
 		label.text = interactable.interactable_name
+		panel.show()
 	else:
 		label.text = ""
+		panel.hide()
 	
