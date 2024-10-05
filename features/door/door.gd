7889227aa290eb1d69a235838257472980df84b6
LaurynasP <laurynas@puskunigis.eu>
2024-10-05 16:31:22 +0300

    Openable fridge

--- features/door/door.gd
+++ features/door/door.gd
@@ -1,4 +1,4 @@
-class_name Door 
+class_name Door
 extends InteractableObject
 
 var is_open: bool = false;
@@ -6,30 +6,33 @@ var closed_rotation = Vector3()
 var open_rotation = Vector3()
 var open_angle = 90
 var tween_duration = 0.5  # Time it takes to animate the door
+@export var switch_open_direction = true;
 
 
 func _ready() -> void:
-	
 	closed_rotation = self.rotation_degrees
-	open_rotation = closed_rotation + Vector3(0, open_angle, 0)
+	if(switch_open_direction):
+		open_rotation = closed_rotation + Vector3(0, open_angle, 0)
+	else:
+		open_rotation = closed_rotation - Vector3(0, open_angle, 0)
 
 
 func interact():
-	toggle_door()
+	toggle_open()
 	
-func toggle_door():
+func toggle_open():
 	if is_open:
-		close_door()
+		close()
 	else:
-		open_door()
+		open()
 
-func open_door():
+func open():
 	var tween = create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "rotation_degrees", open_rotation, tween_duration)
 	is_open = true 
 
-func close_door():
+func close():
 	var tween = create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "rotation_degrees", closed_rotation, tween_duration)
