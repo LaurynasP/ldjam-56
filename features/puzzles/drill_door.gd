7807594c85adb9c70db8450a46ae4d7b7e0fcdb7
LaurynasP <laurynas@puskunigis.eu>
2024-10-05 21:51:25 +0300

    Drill door

--- features/puzzles/drill_door.gd
+++ features/puzzles/drill_door.gd
@@ -1,22 +1,29 @@
 extends Door
 
 var locked = true
+var drilling = false
 @onready var drill = $Drill
 var animation_duration = 2.0
 
+func _ready():
+	super._ready()
+	drill.hide()
+	
+
 func interact():
-	if(locked):
+	if(locked && !drilling):
 		unlock()
-	else:
+	if(!locked):
 		toggle_open()
 
 func unlock():
+	drilling = true
 	drill.show()
 	var tween = create_tween()
-	tween.tween_property(drill, "rotation_degrees", drill. rotation_degrees + Vector3(2000, 0, 0), animation_duration)
+	tween.tween_property(drill, "rotation_degrees", drill. rotation_degrees + Vector3(4000, 0, 0), animation_duration)
 	
 	
-	await tween
+	await tween.finished
 	locked = false
 	drill.queue_free()
 	print("Unlocking")
