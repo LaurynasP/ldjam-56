766b804a30baa18a5ddc9259194dba667660ae71
LaurynasP <laurynas@puskunigis.eu>
2024-10-06 10:48:37 +0300

    Decorations and add food

--- features/door/door.gd
+++ features/door/door.gd
@@ -7,14 +7,15 @@ var open_rotation = Vector3()
 var open_angle = 90
 
 var tween_duration = 0.5 
+@export var open_direction = Vector3(0,1,0)
 @export var switch_open_direction = true;
 
 func _ready() -> void:
 	closed_rotation = self.rotation_degrees
 	if(switch_open_direction):
-		open_rotation = closed_rotation + Vector3(0, open_angle, 0)
+		open_rotation = closed_rotation + open_direction * open_angle
 	else:
-		open_rotation = closed_rotation - Vector3(0, open_angle, 0)
+		open_rotation = closed_rotation - open_direction * open_angle
 
 
 func interact():
